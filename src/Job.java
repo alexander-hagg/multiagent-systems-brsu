ef88ad8a0cc5c059202c64b8b335e9fcc6f30d91
Alexander Hagg <info@haggdesign.de>
2013-10-24 20:13:27 +0200

    made Job inherit Serializable to enable message insertion

--- src/Job.java
+++ src/Job.java
@@ -1,6 +1,9 @@
+import jade.util.leap.Serializable;
 
-public class Job {
 
+public class Job implements Serializable {
+
+	private static final long serialVersionUID = 4630916579557849268L;
 	String name = null;
 	int duration = 0;
 	public String getName() {
