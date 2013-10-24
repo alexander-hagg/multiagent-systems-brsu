491bf05bb072a3bab3f777b611a22d190af7a191
Alexander Hagg <info@haggdesign.de>
2013-10-24 19:25:43 +0200

    added Job class

---
+++ src/Job.java
@@ -0,0 +1,21 @@
+
+public class Job {
+
+	String name = null;
+	int duration = 0;
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+}
