2067f85f1bed6c07a8935e3e4961a7bb81fbe69d
Alexander Hagg <info@haggdesign.de>
2013-10-29 19:11:57 +0100

    code clean up

--- src/RunJade.java
+++ src/RunJade.java
@@ -49,52 +49,32 @@ public class RunJade {
 
 	public static void main(String[] args){
 
-
-		//1), create the platform (Main container (DF+AMS) + containers + monitoring agents : RMA and SNIFFER)
 		rt=emptyPlatform(containerList);
 
-		//2) create agents and add them to the platform.
 		agentList=createAgents(containerList);
 
 		try {
 			System.out.println("Press a key to start the agents");
 			System.in.read();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		//3) launch agents
 		startAgents(agentList);
 
 	}
 
 
-
-	/**********************************************
-	 * 
-	 * Methods used to create an empty platform
-	 * 
-	 **********************************************/
-
-	/**
-	 * Create an empty platform composed of 1 main container and 3 containers.
-	 * 
-	 * @return a ref to the platform and update the containerList
-	 */
 	private static Runtime emptyPlatform(HashMap<String, ContainerController> containerList){
 
 		Runtime rt = Runtime.instance();
 
-		// 1) create a platform (main container+DF+AMS)
 		Profile pMain = new ProfileImpl(hostname, 8888, null);
 		System.out.println("Launching a main-container..."+pMain);
 		AgentContainer mainContainerRef = rt.createMainContainer(pMain); //DF and AMS are include
 
-		// 2) create the containers
-		containerList.putAll(createContainers(rt));
+		containerList.putAll(createContainers(rt)); 
 
-		// 3) create monitoring agents : rma agent, used to debug and monitor the platform; sniffer agent, to monitor communications; 
 		createMonitoringAgents(mainContainerRef);
 
 		System.out.println("Plaform ok");
@@ -102,39 +82,24 @@ public class RunJade {
 
 	}
 
-	/**
-	 * Create the containers used to hold the agents 
-	 * @param rt The reference to the main container
-	 * @return an Hmap associating the name of a container and its object reference.
-	 * 
-	 * note: there is a smarter way to find a container with its name, but we go fast to the goal here. Cf jade's doc.
-	 */
 	private static HashMap<String,ContainerController> createContainers(Runtime rt) {
 		String containerName;
 		ProfileImpl pContainer;
 		ContainerController containerRef;
-		HashMap<String, ContainerController> containerList=new HashMap<String, ContainerController>();//bad to do it here.
+		HashMap<String, ContainerController> containerList = new HashMap<String, ContainerController>();//bad to do it here.
 
 		System.out.println("Launching containers ...");
 
-		containerName="container1";
+		containerName = "container1";
 		pContainer = new ProfileImpl(null, 8888, null);
 		System.out.println("Launching container "+pContainer);
-		containerRef = rt.createAgentContainer(pContainer); //ContainerController replace AgentContainer in the new versions of Jade.
+		containerRef = rt.createAgentContainer(pContainer);
 		containerList.put(containerName, containerRef);
 
 		System.out.println("Launching containers done");
 		return containerList;
 	}
 
-
-	/**
-	 * create the monitoring agents (rma+sniffer) on the main-container given in parameter and launch them.
-	 *  - RMA agent's is used to debug and monitor the platform;
-	 *  - Sniffer agent is used to monitor communications
-	 * @param mc the main-container's reference
-	 * @return a ref to the sniffeur agent
-	 */
 	private static void createMonitoringAgents(ContainerController mc) {
 
 		System.out.println("Launching the rma agent on the main container ...");
@@ -147,76 +112,50 @@ public class RunJade {
 			e.printStackTrace();
 			System.out.println("Launching of rma agent failed");
 		}
-
-		System.out.println("Launching  Sniffer agent on the main container...");
-		AgentController snif=null;
-
-		try {
-			snif= mc.createNewAgent("sniffeur", "jade.tools.sniffer.Sniffer",new Object[0]);
-			snif.start();
-
-		} catch (StaleProxyException e) {
-			e.printStackTrace();
-			System.out.println("launching of sniffer agent failed");
-
-		}		
-
-
+	
 	}
 
-	/**
-	 *  Creates the agents and add them to the agentList.  agents are NOT started.
-	 *@param containerList :Name and container's ref
-	 *@param sniff : a ref to the sniffeur agent
-	 *@return the agentList
-	 */
 	private static List<AgentController> createAgents(HashMap<String, ContainerController> containerList) {
 		System.out.println("Launching agents...");
 		ContainerController c;
 		String agentName;
-		List<AgentController> agentList=new ArrayList<AgentController>();
+		List<AgentController> agentList = new ArrayList<AgentController>();
 
 		c = containerList.get("container1");
 		agentName="JobSupplier1";
 		try {					
-			Object[] objtab=new Object[]{new String("/home/alex/Documents/FH/MAS/Semester 3/Multiagent/JADE-all-4.3.0/jade/src/multiagent-systems-brsu/jobs")};//used to give informations to the agent
-			AgentController	ag=c.createNewAgent(agentName,multiagent_scheduler.JobSupplierAgent.class.getName(),objtab);
+			Object[] objtab = new Object[]{new String("/home/alex/Documents/FH/MAS/Semester 3/Multiagent/JADE-all-4.3.0/jade/src/multiagent-systems-brsu/jobs")};//used to give informations to the agent
+			AgentController	ag = c.createNewAgent(agentName,multiagent_scheduler.JobSupplierAgent.class.getName(),objtab);
 			agentList.add(ag);
-			System.out.println(agentName+" launched");
+			System.out.println(agentName + " launched");
 		} catch (StaleProxyException e) {
 			e.printStackTrace();
 		}
 
 		agentName="Scheduler1";
 		try {
-			Object[] objtab=new Object[]{};//used to give informations to the agent
-			AgentController	ag=c.createNewAgent(agentName,multiagent_scheduler.SchedulerAgent.class.getName(),objtab);
+			Object[] objtab = new Object[]{};//used to give informations to the agent
+			AgentController	ag = c.createNewAgent(agentName,multiagent_scheduler.SchedulerAgent.class.getName(),objtab);
 			agentList.add(ag);
-			System.out.println(agentName+" launched");
+			System.out.println(agentName + " launched");
 		} catch (StaleProxyException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}	
 
 		agentName="SchedulingVisualizer1";
 		try {					
-			Object[] objtab=new Object[]{};//used to give informations to the agent
-			AgentController	ag=c.createNewAgent(agentName,multiagent_scheduler.SchedulingVisualizer.class.getName(),objtab);
+			Object[] objtab = new Object[]{};//used to give informations to the agent
+			AgentController	ag = c.createNewAgent(agentName,multiagent_scheduler.SchedulingVisualizer.class.getName(),objtab);
 			agentList.add(ag);
-			System.out.println(agentName+" launched");
+			System.out.println(agentName + " launched");
 		} catch (StaleProxyException e) {
 			e.printStackTrace();
 		}
-
 
 		System.out.println("Agents launched...");
 		return agentList;
 	}
 
-	/**
-	 * Start the agents
-	 * @param agentList
-	 */
 	private static void startAgents(List<AgentController> agentList){
 
 		System.out.println("Starting agents...");
@@ -231,11 +170,4 @@ public class RunJade {
 		}
 		System.out.println("Agents started...");
 	}
-
 }
-
-
-
-
-
-
