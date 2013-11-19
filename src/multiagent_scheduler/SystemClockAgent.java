39b6b174f405e3744eaa6cc29d2ad127708498a3
Alexander Hagg <info@haggdesign.de>
2013-11-19 16:41:06 +0100

    added system clock agent

--- src/multiagent_scheduler/SystemClockAgent.java
+++ src/multiagent_scheduler/SystemClockAgent.java
@@ -1,7 +1,68 @@
 package multiagent_scheduler;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
 import jade.core.Agent;
+import jade.core.behaviours.TickerBehaviour;
+import jade.domain.AMSService;
+import jade.domain.FIPAAgentManagement.AMSAgentDescription;
+import jade.domain.FIPAAgentManagement.SearchConstraints;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.lang.acl.UnreadableException;
 
 public class SystemClockAgent extends Agent {
 
+	private static final long serialVersionUID = -2319170144217755318L;
+	private AMSAgentDescription[] agents;
+	private ACLMessage message;
+	protected static int cidCnt = 0;
+	String cidBase;
+
+	protected void setup() {
+		System.out.println("SystemClockAgent "+ getAID().getName() + " is ready.");
+		
+		try {
+            SearchConstraints c = new SearchConstraints();
+            c.setMaxResults (new Long(-1));
+            AMSAgentDescription description = new AMSAgentDescription();
+			agents = AMSService.search( this, description, c );
+		}
+		catch ( Exception e ) {
+            System.out.println( "Problem searching AMS: " + e );
+            e.printStackTrace();
+		}
+
+		addBehaviour(new TickerBehaviour(this, 1000)
+		{
+
+			private static final long serialVersionUID = 2319170146627755318L;
+
+			@Override
+			protected void onTick() {
+				message = new ACLMessage(ACLMessage.INFORM);
+				message.setConversationId( genCID() );
+				message.setContent("tick");
+				for ( AMSAgentDescription agent: agents ) {
+					message.addReceiver( agent.getName() );
+				}
+				send( message );
+			}
+		});	
+
+	}
+	
+	protected void takeDown() {
+		System.out.println("SystemClockAgent " + getAID().getName() + " terminating.");
+	}
+	
+	protected String genCID() { 
+		if (cidBase==null) {
+			cidBase = getLocalName() + hashCode() +
+                      System.currentTimeMillis()%10000 + "_";
+		}
+		return cidBase + (cidCnt++); 
+	}
 }
