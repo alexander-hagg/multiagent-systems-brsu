36f5e50706ba9fd32e98732b42479dac396faa4f
Alexander Hagg <info@haggdesign.de>
2013-11-20 16:28:35 +0100

    added schedule execution agent, first version

--- src/multiagent_scheduler/ScheduleExecutionAgent.java
+++ src/multiagent_scheduler/ScheduleExecutionAgent.java
@@ -1,42 +1,18 @@
 package multiagent_scheduler;
 
-import java.io.IOException;
-
 import jade.core.Agent;
-import jade.core.behaviours.CyclicBehaviour;
-import jade.lang.acl.ACLMessage;
-import jade.lang.acl.MessageTemplate;
 
 public class ScheduleExecutionAgent extends Agent{
 
 	private static final long serialVersionUID = 4853132690502168830L;
-	private MessageTemplate templateSchedule;
-	int tickTime = 0;
+	SystemTime ticker;
 
 	protected void setup() {
 		System.out.println("ScheduleExecutionAgent "+ getAID().getName() + " is ready.");
-		
-		templateSchedule = MessageTemplate.MatchPerformative( ACLMessage.INFORM ); 
-		
-		addBehaviour(new ReactToTick(this));	
+		ticker = new SystemTime(this);
+		addBehaviour(ticker);
 	}
 	
-	private class ReactToTick extends CyclicBehaviour
-	{
-		private static final long serialVersionUID = 8693491533114444273L;
-		
-		private ReactToTick(Agent a) {
-			super(a);
-		}
-		public void action()  
-         {
-            ACLMessage msg = receive( templateSchedule );
-            if (msg!=null && msg.getContent().equals("tick")) { 
-                tickTime++;
-                System.out.println("Tick " + tickTime + " ScheduleExecutionAgent");
-            }
-         }
-	}
 	
 	protected void takeDown() {
 		System.out.println("ScheduleExecutionAgent  " + getAID().getName() + " terminating.");
