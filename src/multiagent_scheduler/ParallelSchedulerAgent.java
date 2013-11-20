6c2da01cf7f0c747701f2ee019ccf0fb7650acfd
Alexander Hagg <info@haggdesign.de>
2013-11-20 13:16:35 +0100

    implemented vanilla parallel scheduler agent

--- src/multiagent_scheduler/ParallelSchedulerAgent.java
+++ src/multiagent_scheduler/ParallelSchedulerAgent.java
@@ -1,7 +1,134 @@
 package multiagent_scheduler;
 
-import jade.core.Agent;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 
-public class ParallelSchedulerAgent extends Agent{
+import jade.core.Agent;
+import jade.core.AID;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.SequentialBehaviour;
+import jade.core.behaviours.SimpleBehaviour;
+import jade.lang.acl.MessageTemplate;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.UnreadableException;
+import jade.domain.AMSService;
+import jade.domain.FIPAAgentManagement.*;
+
+public class ParallelSchedulerAgent extends Agent {
+
+	private static final long serialVersionUID = -6918861190459111898L;
+	protected static int cidCnt = 0;
+	String cidBase;
+	private ACLMessage message, reply;
+	private MessageTemplate templateJoblist, templateSchedule;
+	ArrayList<Job> joblist = new ArrayList<Job>();
+	ArrayList<Job> schedule = new ArrayList<Job>();
+	AMSAgentDescription [] agents = null;
+
+	
+	protected void setup() {
+		System.out.println("SchedulerAgent "+ getAID().getName() + " is ready.");
+		
+		templateJoblist = MessageTemplate.and( MessageTemplate.MatchPerformative( ACLMessage.INFORM ),
+	            						MessageTemplate.MatchConversationId( message.getConversationId() ));
+				
+		
+		addBehaviour( new MessageReceiver(this, 1000, templateJoblist )
+        {
+			private static final long serialVersionUID = 8693491577914569273L;
+
+			@SuppressWarnings("unchecked")
+			public void handle( ACLMessage msg ) 
+			{  
+				if (msg == null) 
+					System.out.println("SchedulerAgent: Timeout");
+				else 
+					try {
+						joblist = (ArrayList<Job>) msg.getContentObject();
+						Collections.sort(joblist);
+						for (Job job : joblist) {
+							schedule.add(job);
+						}
+					} catch (UnreadableException e) {
+						e.printStackTrace();
+					}
+           }
+        });
+
+		
+		addBehaviour(new CyclicBehaviour(this)
+		{
+			private static final long serialVersionUID = 8693491533424444273L;
+
+			public void action()  
+	         {
+	            ACLMessage msg = receive( templateSchedule );
+	            templateSchedule = MessageTemplate.MatchPerformative( ACLMessage.QUERY_REF );
+	            if (msg!=null&& msg.getContent().equals("schedule")) { 
+	                reply = msg.createReply();
+	                reply.setConversationId(genCID());
+	                reply.setPerformative( ACLMessage.INFORM );
+
+	                try {
+	                	
+						reply.setContentObject(schedule);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+	                send(reply);
+	            }
+	            block();
+	         }
+		});
+		
+		
+		try {
+            SearchConstraints c = new SearchConstraints();
+            c.setMaxResults (new Long(-1));
+            AMSAgentDescription description = new AMSAgentDescription();
+			agents = AMSService.search( this, description, c );
+		}
+		catch ( Exception e ) {
+            System.out.println( "Problem searching AMS: " + e );
+            e.printStackTrace();
+		}
+		
+		message = newMsg( ACLMessage.QUERY_REF );
+		message.setContent("joblist?");
+		
+		for (AMSAgentDescription agent: agents) {
+			message.addReceiver( agent.getName() );
+		}
+		
+		send ( message );
+	}
+	
+	protected void takeDown() {
+		System.out.println("SchedulingVisualizer " + getAID().getName() + " terminating.");
+	}
+	
+	protected String genCID() { 
+		if (cidBase==null) {
+			cidBase = getLocalName() + hashCode() +
+                      System.currentTimeMillis()%10000 + "_";
+		}
+		return cidBase + (cidCnt++); 
+	}
+	
+	protected ACLMessage getMessage() { return message; }
+	
+	ACLMessage newMsg( int perf, String content, AID dest) {
+		ACLMessage msg = newMsg(perf);
+		if (dest != null) msg.addReceiver( dest );
+			msg.setContent( content );
+		return msg;
+	}
+
+	ACLMessage newMsg( int perf) {
+		ACLMessage msg = new ACLMessage(perf);
+		msg.setConversationId( genCID() );
+		return msg;
+	}
 
 }
